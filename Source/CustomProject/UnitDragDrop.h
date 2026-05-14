4956269f649c894bbb2615b08c81e7ee06d69d12
Thanh Long <tngu0502@student.monash.edu>
2026-05-14 19:41:19 +1000

    add unit drag drop operation

---
+++ Source/CustomProject/UnitDragDrop.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "CoreMinimal.h"
+#include "Blueprint/DragDropOperation.h"
+#include "UnitDragDrop.generated.h"
+
+UCLASS()
+class CUSTOMPROJECT_API UUnitDragDrop : public UDragDropOperation
+{
+    GENERATED_BODY()
+
+public:
+    // The unit being dragged
+    UPROPERTY()
+    AUnit* DraggedUnit = nullptr;
+
+    // Whether unit came from board or bench
+    UPROPERTY()
+    bool bFromBoard = false;
+};
